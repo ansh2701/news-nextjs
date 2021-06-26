15eacc64e9707662382927cf8ba74c31618c84f6
ansh2701 <agrawalansh2701@gmail.com>
2021-06-26 12:45:14 +0530

    fix bugs

--- components/News.js
+++ components/News.js
@@ -1,10 +1,6 @@
-import Cards from "./cards";
-import Image from "next/image";
+import Cards from "./Cards";
 import styles from "../styles/News.module.css";
 const News = ({ posts }) => {
-  //   console.log(posts.posts);
-  //   const { news } = posts;
-  //   console.log(news);
   return (
     <div className={styles.contentWrapper}>
       {posts.map((news) => (
